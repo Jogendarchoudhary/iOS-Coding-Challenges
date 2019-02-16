6ecbdaa9ac078c6dc61ef435d1fb27accfff0470
Jogendar Singh <josingh@blackberry.com>
2019-02-16 18:34:32 +0530

    Challenge: Maximum difference between two elements

---
+++ Maximum_difference_between_two_elements.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//Maximum difference between two elements such that larger element appears after the smaller number
+//Input : arr = {2, 3, 10, 6, 4, 8, 1}
+//Output : 8
+//Explanation : The maximum difference is between 10 and 2.
+
+func MaxDiff(a: [Int]) -> Int {
+    let diff = a.reduce((a[0], 0), {(min($0.0, $1), max($0.1, $1 - $0.0))})
+    return diff.1 > 0 ? diff.1 : -1
+}
+
+print(MaxDiff(a: [2,3,10,6,4,8,1]))
